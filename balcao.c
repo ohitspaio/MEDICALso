8ac77490ca7db9c821787fdc7fcea26d22372175
paio the allmighty <ze.bernardo1999@gmail.com>
2021-11-20 02:40:13 +0000

    base

--- balcao.c
+++ balcao.c
@@ -1,1 +1,19 @@
 #include <balcao.h>
+#include <cliente.h>
+
+char name [30];
+char sintomas [50];
+
+void main() {
+
+    printf("Bem vindo ao sistema Administrador");
+
+    printf("Nome do utente");
+    scanf("&d", &name);
+
+    printf("Sintomas do utente");
+    scanf("&d", &sintomas);
+
+    printf("Nome do utente/", name);
+    printf("Sintomas do utente/", sintomas);
+};
